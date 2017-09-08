cd8ccaebb6054c84d5fa373834d2652cbc6ae109
Stephen Phillips <chengdu.scp@gmail.com>
2017-09-08 16:16:15 -0400

    Made paths more explicit

--- init.m
+++ init.m
@@ -1,1 +1,7 @@
-addpath(genpath('.'))
+curdir = cd;
+addpath(genpath(sprintf('%s/Classes',curdir)))
+addpath(genpath(sprintf('%s/Data',curdir)))
+addpath(genpath(sprintf('%s/Tests',curdir)))
+addpath(genpath(sprintf('%s/Util',curdir)))
+
+
